651eca42489cc3856ec63d452fa1aea8d637df6e
Katelyn Curtiss <153852200+KatelynCurtiss@users.noreply.github.com>
2025-01-08 10:23:10 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+# Katelyn Curtiss
+# January 8 2025
+
+def get_city():
+    city = input("Enter the city you live in:")
+    return city
+
+def get_age():
+    age = input("Enter your age:")
+    return age
+
+def build_output(city, age):
+    output =  f"You live in {city} and you are {age} years old."
+    return output
+
+def main():
+    city = get_city()
+    age = get_age()
+
+    result = build_output(city, age)
+
+    print(result)
+
+if __name__ == "main": 
+  main()
