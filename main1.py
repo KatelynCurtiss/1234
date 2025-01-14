cc0c0bbea33ecd426d8c838230308161e13751b5
Katelyn Curtiss <153852200+KatelynCurtiss@users.noreply.github.com>
2025-01-14 10:38:39 -0500

    Update main1.py

--- main1.py
+++ main1.py
@@ -1,19 +1,19 @@
 # Katelyn Curtiss 
-# January 13 2024
+# January 13 2025
 # Local Scope Challenges
 
-def greet(fname):
-    print(f'Hello {fname}.')
+# def greet(fname):
+#     print(f'Hello {fname}.')
 
 
-def count_vowels(word, vowel):
+def count_vowels(word):
     vowels= "aeiou"
     count = 0
 
     for char in word:
-        if char.lower() in vowel:
+        if char.lower() in vowels:
             count += 1
-            return count 
+    return count 
         
 result = count_vowels("Goldfish")
 print(f"The number of vowels in 'Goldfish' is {result}")
