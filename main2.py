350164b3289eebfb4a95b12a6bbf84af08f2643a
Katelyn Curtiss <153852200+KatelynCurtiss@users.noreply.github.com>
2025-01-14 10:38:34 -0500

    Create main2.py
    updated

---
+++ main2.py
@@ -0,0 +1,10 @@
+# Katelyn Curtiss
+# January 14 2025
+# Returning Multiple Values from a Pyhton Function
+
+def sum_product(sum, product):
+    sum = sum + sum
+    product = product * product 
+
+sum_product()
+
